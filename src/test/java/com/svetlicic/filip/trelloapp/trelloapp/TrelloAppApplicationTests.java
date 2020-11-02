1b1608d466adc346cf037d3df4bada9de78594ae
Geli4ever <38469600+Geli4ever@users.noreply.github.com>
2020-11-02 02:16:16 +0100

    Initial Commit

---
+++ src/test/java/com/svetlicic/filip/trelloapp/trelloapp/TrelloAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svetlicic.filip.trelloapp.trelloapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
